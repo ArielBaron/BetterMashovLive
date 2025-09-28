a5985e669c0203bf79dcfdb8672ef246cef88e7b
ArielBaron <136897420+ArielBaron@users.noreply.github.com>
2025-09-28 18:26:04 +0300

    Update grades.js

--- backend/mashov/grades.js
+++ backend/mashov/grades.js
@@ -1,9 +1,10 @@
 const mashov = require('mashov-api');
 
 const fetchGrades = async (credentials) => {
-    const { SEMEL, YEAR, userId, PASSWORD } = credentials;
+    const { SEMEL, YEAR, username, PASSWORD } = credentials;
     try {
-        const loginInfo = await mashov.loginToMashov(SEMEL, YEAR, userId, PASSWORD);
+        const loginArray = await mashov.loginToMashov(SEMEL, YEAR, username, PASSWORD);
+        const loginInfo = loginArray[0];
         const grades = await mashov.get(loginInfo, 'grades');
         return grades;
     } catch (error) {
