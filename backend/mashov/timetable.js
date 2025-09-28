ddcc722e376a2992e9767c762d36e3525a6bd94e
ArielBaron <136897420+ArielBaron@users.noreply.github.com>
2025-09-28 18:26:20 +0300

    Update timetable.js

--- backend/mashov/timetable.js
+++ backend/mashov/timetable.js
@@ -1,9 +1,10 @@
 const mashov = require('mashov-api');
 
 const fetchTimetable = async (credentials) => {
-    const { SEMEL, YEAR, userId, PASSWORD } = credentials;
+    const { SEMEL, YEAR, username, PASSWORD } = credentials;
     try {
-        const loginInfo = await mashov.loginToMashov(SEMEL, YEAR, userId, PASSWORD);
+        const loginArray = await mashov.loginToMashov(SEMEL, YEAR, username, PASSWORD);
+        const loginInfo = loginArray[0];
         const timetable = await mashov.get(loginInfo, 'timetable');
         return timetable;
     } catch (error) {
